3c89a6e0379bcb7339a20cbc19763e401f6b5277
Артем Казанцев <akibog@mail.ru>
2020-05-22 15:53:36 +0700

    add start function to plugin (notwork)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 import './style.scss';
-import { sum } from './sum';
 
-alert(sum(1, 1));
+(function( $ ) {
+    $.fn.tooltip = function( options ) {  
+  
+      // Создаём настройки по-умолчанию, расширяя их с помощью параметров, которые были переданы
+      var settings = $.extend( {
+        'location'         : 'top',
+        'background-color' : 'blue'
+      }, options);
+  
+      return this.each(function() {        
+  
+        // Тут пишем код плагина tooltip
+  
+      });  
+    };
+  })( jQuery );
