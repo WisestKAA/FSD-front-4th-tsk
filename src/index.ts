d9996e3471bf2def3dec749932efe395d28de8e9
Артем Казанцев <akibog@mail.ru>
2020-07-15 10:12:46 +0700

    added slider for tests

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ $(document).ready(() => {
   const sliders = document.querySelectorAll('.slider');
 
   sliders.forEach((val => {
-    $(val).SimpleSlider({currentVal: [50,0], step: 10, isRangeLineEnabled: true}); 
+    $(val).SimpleSlider({currentVal: [50,0], step: 10, isRangeLineEnabled: true, isScaleEnabled: true, numberOfScaleMarks: 5}); 
   }));
 
   const sliderV = document.querySelector('.slider-v');
   $(sliderV).SimpleSlider({isHorizontal: false, minVal: -100});
 
   const sliderR = document.querySelector('.slider-r');
-  $(sliderR).SimpleSlider({isRange: true, currentVal: [10, 80], isRangeLineEnabled: true});
+  $(sliderR).SimpleSlider({isRange: true, currentVal: [10, 80], isRangeLineEnabled: true, isScaleEnabled: true, numberOfScaleMarks: 5});
   const sliderRV = document.querySelector('.slider-rv');
-  $(sliderRV).SimpleSlider({isRange: true, currentVal: [-90, 50], isRangeLineEnabled: true, minVal:-100, maxVal: 100, isHorizontal: false});
+  $(sliderRV).SimpleSlider({isRange: true, currentVal: [-90, 50], isRangeLineEnabled: true, minVal:-100, maxVal: 100, isHorizontal: false, isScaleEnabled: true, numberOfScaleMarks: 3});
 });
